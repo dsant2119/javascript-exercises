fe1e4d7647686ea336e107e132ae402712f6d506
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2024-10-20 16:38:40 -0700

    Updated function definition to pass test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    newString = '';
+    for(i = 0; i < num; i++){
+        newString += word;
+    }
 
+    return newString;
 };
 
 // Do not edit below this line
