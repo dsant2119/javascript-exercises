432867db1d59530d9e799e290c1d902b6dbc3668
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2024-10-20 16:49:32 -0700

    Update repeatString to pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,13 @@
 const repeatString = function(word, num) {
+    if (num < 0)
+        return 'ERROR';
+    else if (num === 0)
+        return '';
+
     newString = '';
-    for(i = 0; i < num; i++){
+
+    for(i = 0; i < num; i++)
         newString += word;
-    }
 
     return newString;
 };
