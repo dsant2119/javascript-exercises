5c0a024e48f54b5c575253eac67ca78f8806e35d
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2024-10-20 16:51:16 -0700

    repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,7 @@
 const repeatString = function(word, num) {
-    if (num < 0)
-        return 'ERROR';
-    else if (num === 0)
-        return '';
-
+    if (num < 0)    return 'ERROR';
     newString = '';
-
+    
     for(i = 0; i < num; i++)
         newString += word;
 
