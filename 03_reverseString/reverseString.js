1346fc9f65da9c5ec2b313b7ee1bce95446e2555
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2024-10-20 17:40:09 -0700

    Update function definition w recursive solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    if(word.length <= 1)
+        return word;
 
+    return  reverseString(word.slice(1)) + word.charAt(0);
 };
+
+console.log(reverseString('word'));
 
 // Do not edit below this line
 module.exports = reverseString;
