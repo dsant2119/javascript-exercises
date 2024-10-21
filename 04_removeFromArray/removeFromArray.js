72696c4adf919b599509df78b95c0bb211f79653
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2024-10-20 18:17:29 -0700

    Update function definition to pass test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, numToRemove) {
+    newArr = arr.slice(0, arr.indexOf(numToRemove)).concat(arr.slice(arr.indexOf(numToRemove) + 1));
+    // console.log(arr.indexOf(numToRemove));
+    return newArr;
 };
+console.log(removeFromArray([1,2,3,4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
