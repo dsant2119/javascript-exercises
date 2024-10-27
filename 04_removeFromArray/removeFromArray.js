976f7fe668a0279c16234960a8b52ebc26846a76
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2024-10-27 16:55:52 -0700

    More work done to function - not complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,14 +2,18 @@ const removeFromArray = function(arr, ...searchArgs) {
     // argumentsArr = searchArgs;
     console.log("Starting array: ", arr);
     console.log("Elements to be removed array: ", searchArgs);
+    startArr = arr;
     newArr = [];
+    startIndex = 0;
+    num = -1;
     searchArgs.forEach(element => {
         console.log("Checking for: " + element);
-        if(arr.includes(element)){
-            console.error(arr.slice(0, arr.indexOf(element)));
-            newArr.push(arr.slice(0, arr.indexOf(element)));
+        if(startArr.includes(element)){
+            console.log(startArr.slice(startIndex, startArr.indexOf(element)).concat(startArr.slice(startArr.indexOf(element) + 1)));
+            newArr = newArr.concat(startArr.slice(startIndex, startArr.indexOf(element)).concat(startArr.slice(startArr.indexOf(element) + 1)));
+            console.log("Start index: " + startIndex);
         }
-        console.log(newArr);
+        console.log("New array: ", newArr);
     });
     // newArr = arr.slice(0, arr.indexOf(numToRemove)).concat(arr.slice(arr.indexOf(numToRemove) + 1));
     console.log(newArr);
