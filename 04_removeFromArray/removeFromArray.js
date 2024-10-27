11d5d922cb57020af611ea887d9ee16e2ad4bc85
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2024-10-27 16:25:30 -0700

    Update work done to function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,21 @@
-const removeFromArray = function(arr, numToRemove) {
-    newArr = arr.slice(0, arr.indexOf(numToRemove)).concat(arr.slice(arr.indexOf(numToRemove) + 1));
-    // console.log(arr.indexOf(numToRemove));
+const removeFromArray = function(arr, ...searchArgs) {
+    // argumentsArr = searchArgs;
+    console.log("Starting array: ", arr);
+    console.log("Elements to be removed array: ", searchArgs);
+    newArr = [];
+    searchArgs.forEach(element => {
+        console.log("Checking for: " + element);
+        if(arr.includes(element)){
+            console.error(arr.slice(0, arr.indexOf(element)));
+            newArr.push(arr.slice(0, arr.indexOf(element)));
+        }
+        console.log(newArr);
+    });
+    // newArr = arr.slice(0, arr.indexOf(numToRemove)).concat(arr.slice(arr.indexOf(numToRemove) + 1));
+    console.log(newArr);
     return newArr;
 };
-console.log(removeFromArray([1,2,3,4], 3));
+removeFromArray([1,2,3,4], 3, 4);
 
 // Do not edit below this line
 module.exports = removeFromArray;
