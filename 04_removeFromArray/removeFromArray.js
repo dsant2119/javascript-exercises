f7cb1ca65801f7ef65dfdbca5ec5cbf39f1395a7
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2024-11-02 18:25:25 -0700

    Implement solution for first 2 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,22 +1,14 @@
 const removeFromArray = function(arr, ...searchArgs) {
-    // argumentsArr = searchArgs;
     console.log("Starting array: ", arr);
     console.log("Elements to be removed array: ", searchArgs);
-    startArr = arr;
     newArr = [];
-    startIndex = 0;
-    num = -1;
-    searchArgs.forEach(element => {
-        console.log("Checking for: " + element);
-        if(startArr.includes(element)){
-            console.log(startArr.slice(startIndex, startArr.indexOf(element)).concat(startArr.slice(startArr.indexOf(element) + 1)));
-            newArr = newArr.concat(startArr.slice(startIndex, startArr.indexOf(element)).concat(startArr.slice(startArr.indexOf(element) + 1)));
-            console.log("Start index: " + startIndex);
-        }
-        console.log("New array: ", newArr);
+
+    arr.forEach(element => {
+        if(!searchArgs.includes(element))
+            newArr.push(element);
     });
-    // newArr = arr.slice(0, arr.indexOf(numToRemove)).concat(arr.slice(arr.indexOf(numToRemove) + 1));
-    console.log(newArr);
+
+    console.log("New array final: ",  newArr);
     return newArr;
 };
 removeFromArray([1,2,3,4], 3, 4);
