59daf4f69a2bab0f0cc4870cb38955260835cb41
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2024-11-03 16:12:49 -0800

    Implement formulae and enable first 2 tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
 const convertToCelsius = function(temp) {
-
+  const temp_C = ((temp - 32) * (5/9));
+  console.log(temp_C);
+  return temp_C;
 };
 
 const convertToFahrenheit = function(temp) {
-  
+  const temp_F = ((temp *(5/9) + 32));
+  console.log(temp_F);
+  return temp_F;
 };
 
 // Do not edit below this line
