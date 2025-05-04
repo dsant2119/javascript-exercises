c444b69cce4cedd05b7bfd848d9bd94952c4f049
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2025-05-03 18:01:14 -0700

    Add prettier config 4 spaces

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,22 +1,24 @@
 const palindromes = function (str) {
-  let start = 0;
-  let end = str.length - 1;
-  let pal = false;
+    // Process string
 
-  if (str.length % 2 != 0)
-    while (start != end) {
-      start++;
-      end--;
-      if (str[start] === str[end]) pal = true;
-    }
-  else
-    while (start != end - 1) {
-      counter++;
-      start++;
-      end--;
-      if (str[start] === str[end]) pal = true;
-    }
-  return pal;
+    let start = 0;
+    let end = str.length - 1;
+    let pal = false;
+
+    if (str.length % 2 != 0)
+        while (start != end) {
+            start++;
+            end--;
+            if (str[start] === str[end]) pal = true;
+        }
+    else
+        while (start != end - 1) {
+            counter++;
+            start++;
+            end--;
+            if (str[start] === str[end]) pal = true;
+        }
+    return pal;
 };
 
 // Do not edit below this line
