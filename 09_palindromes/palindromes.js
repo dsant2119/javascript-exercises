02ef5ce239b6001794be46380a3b3b164c6dc965
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2025-05-03 17:56:00 -0700

    Single words work

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let start = 0;
+  let end = str.length - 1;
+  let pal = false;
 
+  if (str.length % 2 != 0)
+    while (start != end) {
+      start++;
+      end--;
+      if (str[start] === str[end]) pal = true;
+    }
+  else
+    while (start != end - 1) {
+      counter++;
+      start++;
+      end--;
+      if (str[start] === str[end]) pal = true;
+    }
+  return pal;
 };
 
 // Do not edit below this line
