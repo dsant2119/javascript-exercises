f122bb5f6e313d07973f2d9935a2f554e703124a
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2025-05-10 17:34:21 -0700

    Create loop version of fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = (num) => {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        if (i === 0 || i === 1) arr.push(1);
+        else arr.push(arr[i - 2] + arr[i - 1]);
+    }
 
+    return arr[num - 1];
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
