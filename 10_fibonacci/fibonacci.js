e7c9c36f7f0e9f25a6a3c8f5dfb437dc32acf4f7
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2025-05-10 17:43:30 -0700

    Pass all tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,14 +1,16 @@
 const fibonacci = (num) => {
     let arr = [];
-    for (let i = 0; i < num; i++) {
+    let number = Number(num);
+    if (number === 0) return 0;
+    if (number < 0) return "OOPS";
+
+    for (let i = 0; i < number; i++) {
         if (i === 0 || i === 1) arr.push(1);
         else arr.push(arr[i - 2] + arr[i - 1]);
     }
 
-    return arr[num - 1];
+    return arr[number - 1];
 };
-
-console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
