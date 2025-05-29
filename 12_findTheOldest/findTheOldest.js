41ae11e41d8b30a75e69ef7137ab85c12e8ffc47
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2025-05-29 01:13:15 -0700

    Begin initial function writing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+];
 
+const findTheOldest = function (array) {
+    let ageToBeat = calculateAge(array[0]);
+    array.forEach((person) => {
+        if (calculateAge(person) > ageToBeat) return person;
+        else return array[0];
+    });
 };
+
+const calculateAge = (obj) => {
+    return obj.yearOfDeath - obj.yearOfBirth;
+};
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
