0172cf968191eec3450d6e5450bb4e71925fc065
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2025-05-29 02:02:41 -0700

    Must fix function logic

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,21 @@
 const findTheOldest = function (array) {
     const sorted = array.sort((a, b) => {
-        const ageA = a.yearOfDeath - a.yearOfBirth;
-        const ageB = b.yearOfDeath - b.yearOfBirth;
+        const currentDate = new Date().getFullYear();
+        const ageA = -111;
+        const ageB = -111;
+
+        if (a.yearOfDeath != undefined && b.yearOfDeath != undefined) {
+            const ageA = a.yearOfDeath - a.yearOfBirth;
+            const ageB = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            const ageA = currentDate - a.yearOfBirth;
+            const ageB = currentDate - b.yearOfBirth;
+        }
         return ageB - ageA;
     });
 
     return sorted[0];
 };
-
-console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
