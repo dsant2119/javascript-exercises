8c923fd7c813e2fb163dc6afdd02c54a2aeab340
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2025-05-29 01:48:45 -0700

    Complete first test and open up second test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,31 +1,11 @@
-const people = [
-    {
-        name: "Carly",
-        yearOfBirth: 1942,
-        yearOfDeath: 1970,
-    },
-    {
-        name: "Ray",
-        yearOfBirth: 1962,
-        yearOfDeath: 2011,
-    },
-    {
-        name: "Jane",
-        yearOfBirth: 1912,
-        yearOfDeath: 1941,
-    },
-];
-
 const findTheOldest = function (array) {
-    let ageToBeat = calculateAge(array[0]);
-    array.forEach((person) => {
-        if (calculateAge(person) > ageToBeat) return person;
-        else return array[0];
+    const sorted = array.sort((a, b) => {
+        const ageA = a.yearOfDeath - a.yearOfBirth;
+        const ageB = b.yearOfDeath - b.yearOfBirth;
+        return ageB - ageA;
     });
-};
 
-const calculateAge = (obj) => {
-    return obj.yearOfDeath - obj.yearOfBirth;
+    return sorted[0];
 };
 
 console.log(findTheOldest(people));
